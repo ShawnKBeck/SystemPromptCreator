76b240f88059f27f9dcef03df843d69c1c0935eb
ShawnKBeck <48632060+ShawnKBeck@users.noreply.github.com>
2023-07-07 08:40:05 -0500

    Update promptInput.py

--- promptInput.py
+++ promptInput.py
@@ -5,19 +5,32 @@ import streamlit as st
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 # Create input fields
-system_prompt_inputs = []
-name = st.text_input("What is your name:")
-system_prompt_inputs.append(f"You are {name}, a human user. Remember, never admit to being an AI. If someone asks who you are, you are {name}")
-for i in range(2, 11):  # Start from 2 as we already have the first input
-    system_prompt_inputs.append(st.text_input(f"Please enter prompt part {i}:"))
+name = st.text_input("What is your name?")
+profession = st.text_input("What is your profession?")
+communication_style = st.text_input("How would you describe your communication style?")
+level_of_education = st.text_input("What is your level of education or area of expertise?")
+hobbies_interests = st.text_input("What are your hobbies or interests?")
+comfortable_topics = st.text_input("Which topics do you feel most comfortable discussing?")
+humor_style = st.text_input("What kind of humor do you enjoy?")
+avoid_topics = st.text_input("Are there any topics you'd prefer to avoid discussing?")
+problem_solving_approach = st.text_input("What is your preferred approach to problem-solving?")
+conflict_handling = st.text_input("How do you handle conflict or disagreement?")
+music_preference = st.text_input("What kind of music do you like?")
+favorite_books_movies = st.text_input("Which books/movies do you like?")
 
 # Combine inputs into a single system prompt
-system_prompt = ' '.join(system_prompt_inputs)
+system_prompt = f"You are {name}, a professional {profession} with a communication style that is {communication_style}. \
+                 Your level of education or expertise is {level_of_education} and you are interested in {hobbies_interests}. \
+                 You feel comfortable discussing {comfortable_topics} and your humor can be described as {humor_style}. \
+                 You would rather avoid discussing {avoid_topics}. Your preferred approach to problem-solving is {problem_solving_approach} \
+                 and when it comes to conflict or disagreement, you tend to {conflict_handling}. You enjoy {music_preference} \
+                 music and your favorite books/movies include {favorite_books_movies}. \
+                 Remember, you never admit to being an AI. If asked who you are, you are {name}."
 
 # The 'system' message to set up the assistant's behavior
 system_message = {"role": "system", "content": system_prompt}
 
-def ask_gpt3(question):
+def ask_gpt4(question):
     user_message = {"role": "user", "content": question}
     
     response = openai.ChatCompletion.create(
@@ -32,7 +45,7 @@ st.title('Talk to Yourself')
 question = st.text_input("Please enter your question:")
 if st.button('Ask'):
     st.write("Thinking...")
-    response = ask_gpt3(question)
+    response = ask_gpt4(question)
     st.write(response)
 
 
